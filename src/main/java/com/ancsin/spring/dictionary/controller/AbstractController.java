130e7c28af6ed2edc3b18804fcde72b85d39d239
Daniel Ancsin <ancsin.daniel@gmail.com>
2020-10-30 19:29:29 +0100

    added AbstractController, modified UserController to utilise it

--- src/main/java/com/ancsin/spring/dictionary/controller/AbstractController.java
+++ src/main/java/com/ancsin/spring/dictionary/controller/AbstractController.java
@@ -1,36 +1,112 @@
 package com.ancsin.spring.dictionary.controller;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ancsin.spring.dictionary.entity.abstracts.AbstractEntity;
 import com.ancsin.spring.dictionary.service.interfaces.AbstractService;
 
-//@Controller
-//@RequestMapping("/abstract")
 public abstract class AbstractController<T extends AbstractEntity> {
-
+	
+	// store the type of the generic class
+	private Class<T> type;
+	
+	// store the class name of the generic class
+	protected String className;
+			
 	@Autowired
 	private AbstractService<T> abstractService;
+	
+	// TODO : make it more flexible (individually set every mapping and entity name
+	public AbstractController(AbstractService<T> abstractService, Class<T> type) {
+		this(abstractService, type, type.getSimpleName().toLowerCase());
+	}
+	
+	public AbstractController(AbstractService<T> abstractService, Class<T> type, String mappingAndEntitiesName) {
+		this.abstractService = abstractService;
+		this.type = type;
+		this.className = mappingAndEntitiesName;
+	}
+	
+	@GetMapping({"","/"})
+	public String list(Model model) {
+		
+		List<T> entities = abstractService.findAll();
+		
+		// add to the spring model
+		model.addAttribute(className+"s", entities);
+		
+		return "/" + className + "s";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		T entity;
+		try {
+			entity = (T) type.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | 
+				InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			// TODO : log the error, warn the user
+			e.printStackTrace();
+			return "/" + className + "s";
+		}
+		
+		model.addAttribute(className, entity);
+		
+		return "/" + className + "-form";
+	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<T> list() { return abstractService.findAll(); }
-
-	@RequestMapping(method = RequestMethod.POST)
-	public T create(@RequestBody T entity) { return abstractService.save(entity); }
-
-	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
-	public T update(@PathVariable(value = "id") long id, @RequestBody T entity) { return abstractService.save(entity); }
-
-	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") long id) { abstractService.deleteById(id); }
-
-	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public T get(@PathVariable(value = "id") long id) { return abstractService.findById(id); }
-
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("id") long id, Model model) {
+		
+		// get the Entity from the service
+		T entity = abstractService.findById(id);
+		
+		// set Entity as a model attribute to pre-populate the form
+		model.addAttribute(className, entity);
+		
+		// send over to the form
+		return "/" + className + "-form";
+	}
+	
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute T entity) {
+		
+		// save the Entity
+		abstractService.save(entity);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/" + className + "s";
+	}
+	
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam long id) {
+		
+		// delete the Entity
+		abstractService.deleteById(id);
+		
+		return "redirect:/" + className + "s";
+	}
+	
+	@GetMapping("/search")
+	public String search(@RequestParam("name") String name, Model model) {
+		
+		List<T> entities = abstractService.searchBy(name);
+		
+		// add to the spring model
+		model.addAttribute(className + "s", entities);
+		
+		return "/" + className + "s";
+	}
+	
 }
--- src/main/java/com/ancsin/spring/dictionary/controller/UserController.java
+++ src/main/java/com/ancsin/spring/dictionary/controller/UserController.java
@@ -1,94 +1,17 @@
 package com.ancsin.spring.dictionary.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ancsin.spring.dictionary.entity.User;
-import com.ancsin.spring.dictionary.service.interfaces.UserService;
+import com.ancsin.spring.dictionary.service.interfaces.AbstractService;
 
 @Controller
 @RequestMapping("/users")
-public class UserController {
+public class UserController extends AbstractController<User> {
 
-	private UserService userService;
-
-	public UserController(UserService userService) {
-		this.userService = userService;
+	public UserController(AbstractService<User> userService) {
+		super(userService, User.class);
 	}
 	
-	@GetMapping({"","/"})
-	public String list(Model model) {
-		
-		// get Users from db
-		List<User> users = userService.findAll();
-		
-		// add to the spring model
-		model.addAttribute("users", users);
-		
-		return "/users";
-	}
-	
-	@GetMapping("/showFormForAdd")
-	public String showFormForAdd(Model model) {
-		
-		// create model attribute to bind form data
-		User user = new User();
-		
-		model.addAttribute("user", user);
-		
-		return "/user-form";
-	}
-
-	@GetMapping("/showFormForUpdate")
-	public String showFormForUpdate(@RequestParam("userId") long id, Model model) {
-		
-		// get the User from the service
-		User user = userService.findById(id);
-		
-		// set User as a model attribute to pre-populate the form
-		model.addAttribute("user", user);
-		
-		// send over to our form
-		return "/user-form";			
-	}
-	
-	
-	@PostMapping("/save")
-	public String save(@ModelAttribute("user") User user) {
-		
-		// save the User
-		userService.save(user);
-		
-		// use a redirect to prevent duplicate submissions
-		return "redirect:/users";
-	}
-	
-	
-	@GetMapping("/delete")
-	public String delete(@RequestParam("userId") long id) {
-		
-		// delete the User
-		userService.deleteById(id);
-		
-		return "redirect:/users";
-	}
-	
-	@GetMapping("/search")
-	public String search(@RequestParam("name") String name, Model model) {
-		
-		// delete the User
-		List<User> users = userService.searchBy(name);
-		
-		// add to the spring model
-		model.addAttribute("users", users);
-		
-		return "/users";
-	}
 }
